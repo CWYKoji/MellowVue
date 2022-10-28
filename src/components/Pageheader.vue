39bed83f11a950732b77b21808d662aa4ff12f84
Cheuk Wah Yu <yucheukwah@gmail.com>
2022-10-28 17:00:43 +0100

    V0.11

--- src/components/Pageheader.vue
+++ src/components/Pageheader.vue
@@ -5,7 +5,7 @@
 
 <template>
 
-<header class ="siteHeader">
+<header id ="siteHeader">
     <h1>Mellow</h1>
 </header>   
 
@@ -17,11 +17,18 @@
 
 <style scoped>
 
-.siteHeader{
+#siteHeader{
+position: fixed;
+top: 0;
+width: 100%;
+height: 80px;
+background: blue;
+z-index: 50;  
+left: 0;  
+}
 
-    height: 75px;
-    background-color: blanchedalmond;
-    
+h1{
+    color: greenyellow;
 }
 
 
