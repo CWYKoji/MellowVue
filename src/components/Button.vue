2638ec0424e1c900f1b6b35e74bb055f812cbe83
Cheuk Wah Yu <yucheukwah@gmail.com>
2022-10-28 16:19:32 +0100

    Master

---
+++ src/components/Button.vue
@@ -0,0 +1,4 @@
+<template>
+
+    <button>click me</button>
+</template>
---
+++ src/components/Pageheader.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+
+<template>
+
+<header class ="siteHeader">
+    <h1>Mellow</h1>
+</header>   
+
+
+
+
+</template>
+
+
+<style scoped>
+
+.siteHeader{
+
+    height: 75px;
+    background-color: blanchedalmond;
+    
+}
+
+
+</style>
