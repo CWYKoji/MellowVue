e3b3d2e1fc9ea9c263ca1a55852a960cfd4818eb
Cheuk Wah Yu <yucheukwah@gmail.com>
2022-10-31 16:57:14 +0000

    0.2

---
+++ src/components/TDListItem.vue
@@ -0,0 +1,24 @@
+
+<script setup>
+const props = defineProps({
+    task:{
+        type: String,
+    },
+    completed: {
+        type: Boolean
+    }
+
+})
+
+</script>
+<template>
+    <div class ="listItem">{{props.task}}</div>
+    
+</template>
+
+<style scoped>
+.completed{
+    text-decoration: line-through;
+}
+
+</style>
