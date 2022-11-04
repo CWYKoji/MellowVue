94682eb97ee5157118e57b257fd96aadf1eb5420
CWYKoji <115834729+CWYKoji@users.noreply.github.com>
2022-11-04 16:24:25 +0000

    1.11

--- src/components/BoardListItem.vue
+++ src/components/BoardListItem.vue
@@ -2,21 +2,15 @@
 import Card from './Card.vue'
 import { ref } from 'vue'
 
-
-
-
 const props = defineProps({
     task:{
         type: String,
     },
-    
-
 })
 
 </script>
 <template>
         <Card></Card>
-    
 </template>
 
 <style scoped>
